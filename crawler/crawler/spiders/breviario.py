6e341b2f7198f1baa261cd0b2bb4119f3c653290
Guilherme-Valle <guidovalle1608@gmail.com>
2021-05-03 16:05:21 -0300

    Ajusta crawler

--- crawler/crawler/spiders/breviario.py
+++ crawler/crawler/spiders/breviario.py
@@ -1,19 +1,24 @@
 import scrapy
+from datetime import date
+import locale
 from scrapy.http import Request
+
 
 class BreviarioSpider(scrapy.Spider):
     name = 'breviario_spider'
     start_urls = ['https://rumoasantidade.com.br/livro-breviario-confianca']
 
     def parse(self, response):
-        url = response.xpath("//*[contains(text(), '2 de Janeiro')]").css('a::attr(href)').get()
+        locale.setlocale(locale.LC_TIME, "pt_BR")
+        date_parse = date(2020, self.selected_date.split('/')[1], self.selected_date.split('/')[0]).strftime(
+            "%-d de %B")
+        url = response.xpath(f"//*[contains(text(), '{date_parse}')]").css('a::attr(href)').get()
         # Chama outra função utilizando o url do dia em questão
         yield Request(url=url, callback=self.parse_page)
 
     # Como fazer crawl nesta página?
     def parse_page(self, response):
-        text = ' '.join(response.css('div.pf-content').css('p::text').getall())
-        return text
-
-
-
+        yield {
+            'text': ' '.join(response.css('div.pf-content').css('p::text').getall()),
+            'reference': ' '.join(response.css('div.pf-content').css('p::text').getall())
+        }
