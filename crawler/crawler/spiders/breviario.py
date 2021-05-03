5318db5128a09a0b42289be86b2596998cc96164
Guilherme-Valle <guidovalle1608@gmail.com>
2021-05-03 14:35:03 -0300

    Ajusta resp

--- crawler/crawler/spiders/breviario.py
+++ crawler/crawler/spiders/breviario.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.http import Request
 
 class BreviarioSpider(scrapy.Spider):
     name = 'breviario_spider'
@@ -6,8 +7,12 @@ class BreviarioSpider(scrapy.Spider):
 
     def parse(self, response):
         url = response.xpath("//*[contains(text(), '2 de Janeiro')]").css('a::attr(href)').get()
-        print(url)
+        # Chama outra função utilizando o url do dia em questão
+        yield Request(url=url, callback=self.parse_page)
 
+    # Como fazer crawl nesta página? 
+    def parse_page(self, response):
+        return
 
 
 
