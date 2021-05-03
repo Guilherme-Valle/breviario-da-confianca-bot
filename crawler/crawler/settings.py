106a31272fd93bc556928dbd3d047528be83533c
Guilherme-Valle <guidovalle1608@gmail.com>
2021-05-03 16:11:51 -0300

    Ajusta crawler para retornar texto do breviario usando parametro selected_date

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -12,6 +12,7 @@ BOT_NAME = 'crawler'
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'crawler (+http://www.yourdomain.com)'
--- crawler/crawler/spiders/breviario.py
+++ crawler/crawler/spiders/breviario.py
@@ -10,8 +10,7 @@ class BreviarioSpider(scrapy.Spider):
 
     def parse(self, response):
         locale.setlocale(locale.LC_TIME, "pt_BR")
-        date_parse = date(2020, self.selected_date.split('/')[1], self.selected_date.split('/')[0]).strftime(
-            "%-d de %B")
+        date_parse = date(2020, int(self.selected_date.split('/')[1]), int(self.selected_date.split('/')[0])).strftime("%-d de %B")
         url = response.xpath(f"//*[contains(text(), '{date_parse}')]").css('a::attr(href)').get()
         # Chama outra função utilizando o url do dia em questão
         yield Request(url=url, callback=self.parse_page)
@@ -20,5 +19,5 @@ class BreviarioSpider(scrapy.Spider):
     def parse_page(self, response):
         yield {
             'text': ' '.join(response.css('div.pf-content').css('p::text').getall()),
-            'reference': ' '.join(response.css('div.pf-content').css('p::text').getall())
+            'reference': ' '.join(response.css('div.pf-content').css('em::text').getall())
         }
