dc8c511af01b0a77d2cc54dbb66f68d6c940bb68
Guilherme-Valle <guidovalle1608@gmail.com>
2021-05-04 13:20:46 -0300

    Ajusta troca de respfin

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -9,12 +9,12 @@ import json
 def get_today_meditation(update, context):
     chat_id = update.message.chat_id
     today = date.today()
-    os.system('rm ../crawler/crawler/spiders/output.json')
+    os.system('rm ~/breviario-da-confianca-bot/crawler/crawler/spiders/output.json')
     # Rodar comando shell com a data, buscar o JSON, extrair informações, montar string e enviar pelo bot
-    script = 'cd ../crawler/crawler/spiders && scrapy crawl breviario_spider -a selected_date=' + str(today.day) + '/' + str(today.month) + ' -o output.json'
+    script = 'cd ~/breviario-da-confianca-bot/crawler/crawler/spiders && scrapy crawl breviario_spider -a selected_date=' + str(today.day) + '/' + str(today.month) + ' -o output.json'
     os.system(script)
 
-    with open("../crawler/crawler/spiders/output.json") as json_file:
+    with open("~/breviario-da-confianca-bot/crawler/crawler/spiders/output.json") as json_file:
         meditation = json.load(json_file)
 
     text_meditation = "<b>" + meditation[0]['meditation_day'] + " - " + meditation[0]['title'] + " </b> \n \n" + meditation[0]['text'] \
