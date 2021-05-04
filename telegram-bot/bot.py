edcc67dce5d75ed83a0af597aa08b44bb8700ab6
Guilherme-Valle <guidovalle1608@gmail.com>
2021-05-04 09:55:53 -0300

    Finalize core do bot

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -1,13 +1,25 @@
 from telegram.ext import Updater, CommandHandler
+import telegram
 from datetime import date
 import locale
+import os
+import json
 
 
 def get_today_meditation(update, context):
     chat_id = update.message.chat_id
     today = date.today()
     # Rodar comando shell com a data, buscar o JSON, extrair informações, montar string e enviar pelo bot
-    context.bot.send_message(chat_id=chat_id, text=str(today.day) + '/' + str(today.month))
+    script = 'cd ../crawler/crawler/spiders && scrapy crawl breviario_spider -a selected_date=' + str(today.day) + '/' + str(today.month) + ' -o output.json'
+    os.system(script)
+
+    with open("../crawler/crawler/spiders/output.json") as json_file:
+        meditation = json.load(json_file)
+
+    text_meditation = "<b>" + meditation[0]['meditation_day'] + " - " + meditation[0]['title'] + " </b> \n \n" + meditation[0]['text'] \
+                      + "\n \n <i>" + meditation[0]['reference'] + "</i>"
+
+    context.bot.send_message(chat_id=chat_id, text=text_meditation, parse_mode=telegram.ParseMode.HTML)
 
 
 def main():
