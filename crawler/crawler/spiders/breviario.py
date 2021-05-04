f474d802ea56e4ef752cfb17d6b49114a4fd03d3
Guilherme-Valle <guidovalle1608@gmail.com>
2021-05-04 12:27:07 -0300

    Adiciona debug

--- crawler/crawler/spiders/breviario.py
+++ crawler/crawler/spiders/breviario.py
@@ -10,7 +10,10 @@ class BreviarioSpider(scrapy.Spider):
 
     def parse(self, response):
         locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
-        date_parse = '4 de Maio'
+        date_parse = date(2020, int(self.selected_date.split('/')[1]), int(self.selected_date.split('/')[0])).strftime("%-d de %B")
+        date_parse = date_parse.replace('janeiro', 'Janeiro').replace('fevereiro', 'Fevereiro').replace('março', 'Março').replace('abril', 'Abril')\
+            .replace('maio', 'Maio').replace('junho', 'Junho').replace('julho', 'Julho').replace('agosto', 'Agosto').replace('setembro', 'Setembro')\
+            .replace('outubro', 'Outubro').replace('novembro', 'Novembro').replace('dezembro', 'Dezembro')
         url = response.xpath(f"//*[contains(text(), '{date_parse}')]").css('a::attr(href)').get()
         print(date_parse)
         print(url)
