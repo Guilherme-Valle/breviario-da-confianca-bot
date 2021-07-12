ed529cdd2633451e400cbc1fc86835b7bbdac549
Guilherme-Valle <guidovalle1608@gmail.com>
2021-07-12 10:42:57 -0300

    splita busca da meditação em código separado

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -5,28 +5,33 @@ import locale
 import os
 import json
 
-
-def get_today_meditation(update, context):
-    chat_id = update.message.chat_id
-    today = date.today()
-    print(today)
+def get_meditation_from_website(selected_date):
     # Deleta arquivo de output prévio
     os.system('rm -f ~/breviario-da-confianca-bot/crawler/crawler/spiders/output.json || true')
 
-    # Script para rodar o webcrawling, passando como parâmetro a data de hoje
+    # Script para rodar o webcrawling, passando como parâmetro a data
     script = 'cd ~/breviario-da-confianca-bot/crawler/crawler/spiders && /usr/local/bin/scrapy crawl breviario_spider -a selected_date=' \
-             + str(today.day) + '/' + str(today.month) + ' -o output.json'
+             + str(selected_date.day) + '/' + str(selected_date.month) + ' -o output.json'
     os.system(script)
 
     # Processa o arquivo de output gerado pelo crawler, extraindo dele o texto da meditação
     with open(r'/root/breviario-da-confianca-bot/crawler/crawler/spiders/output.json') as json_file:
         meditation = json.load(json_file)
 
-    text_meditation = "<b>" + meditation[0]['meditation_day'] + " - " + meditation[0]['title'] + " </b> \n \n" + meditation[0]['text'] \
+    return "<b>" + meditation[0]['meditation_day'] + " - " + meditation[0]['title'] + " </b> \n \n" + meditation[0]['text'] \
                       + "\n \n <i>" + meditation[0]['reference'] + "</i>"
+
+
+
+def get_today_meditation(update, context):
+    chat_id = update.message.chat_id
+    today = date.today()
+
+    text_meditation = get_meditation_from_website(today)
 
     # Envia a meditação
     context.bot.send_message(chat_id=chat_id, text=text_meditation, parse_mode=telegram.ParseMode.HTML)
+
 
 
 def main():
